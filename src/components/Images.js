9c5778578c860bcedfa473ee3e928e6f3556c734
Roman Kuklin <sk.trac@yandex.ru>
2019-07-03 22:10:17 +0500

    * Add Image component for rendering a single image * Add react-router #1

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,6 +1,8 @@
 import React, {Component} from "react";
 import axios from 'axios';
 import {Row, Col, Alert, Pagination} from 'antd';
+import {Link} from "react-router-dom";
+
 const _ = require('lodash');
 
 export default class Images extends Component {
@@ -8,7 +10,11 @@ export default class Images extends Component {
         super(props);
         this.SEARCH_PHOTOS_URL = 'https://api.unsplash.com/search/photos';
         this.GET_PHOTOS_URL = 'https://api.unsplash.com/photos';
-        this.state = {images: [], isError: false, totalPages: 1, SEARCH_PHOTOS_URL: 'https://api.unsplash.com/search/photos'};
+        this.state = {
+            images: [],
+            isError: false,
+            totalPages: 1,
+        };
     }
 
     componentDidMount() {
@@ -16,7 +22,7 @@ export default class Images extends Component {
             .then((response) => {
                 const images = _.get(response, "data", []);
 
-                if(!Array.isArray(images)) {
+                if (!Array.isArray(images)) {
                     this.setState({isError: true});
                     return;
                 }
@@ -29,7 +35,7 @@ export default class Images extends Component {
     }
 
     componentWillUpdate(nextProps, nextState, nextContext) {
-        if (nextProps.term && nextProps.terms !== this.props.term && nextState.images === this.state.images) {
+        if (nextProps.term && nextProps.term !== this.props.term) {
             this.searchImages(nextProps.term)
         }
     }
@@ -47,7 +53,7 @@ export default class Images extends Component {
                 const images = _.get(response, "data.results", []);
                 const totalPages = _.get(response, "data.total_pages", 1);
 
-                if(!Array.isArray(images) || !Number.isInteger(totalPages)) {
+                if (!Array.isArray(images) || !Number.isInteger(totalPages)) {
                     this.setState({isError: true});
                     return;
                 }
@@ -62,38 +68,38 @@ export default class Images extends Component {
             });
     };
 
-    itemPagination = (page, pageSize) => {
-        this.searchImages(this.props.term, page, pageSize);
-    };
+    itemPagination = (page, pageSize) => this.searchImages(this.props.term, page, pageSize);
 
     render() {
         const {isError = false, images = [], totalPages = 1} = this.state;
 
         return (
-                <Row>
-                    {(isError === true) ?
-                        < Col span={24}>
-                            <Alert
-                                message="Ошибка"
-                                description="Попробуйте проверить соединение с интернетом или перезагрузить"
-                                type="error"
-                                closable
-                            />
-                        </Col>
-                        : null
-                    }
-                    {images.map((image, i) => {
-                        return (
-                            <Col span={8} key={i}>
+            <Row>
+                {(isError === true) ?
+                    < Col span={24}>
+                        <Alert
+                            message="Ошибка"
+                            description="Попробуйте проверить соединение с интернетом или перезагрузить"
+                            type="error"
+                            closable
+                        />
+                    </Col>
+                    : null
+                }
+                {images.map((image, i) => {
+                    return (
+                        <Col span={8} key={i}>
+                            <Link to={"/image/" + _.get(image, "id", '')}>
                                 <img width={320} height={240} src={_.get(image, "urls.small", '')}
                                      alt={_.get(image, "alt_description", '')}/>
-                            </Col>
-                        )
-                    })}
-                    <Col span={24}>
-                        <Pagination defaultCurrent={1} total={totalPages} onChange={this.itemPagination}/>
-                    </Col>
-                </Row>
+                            </Link>
+                        </Col>
+                    )
+                })}
+                <Col span={24}>
+                    <Pagination defaultCurrent={1} total={totalPages} onChange={this.itemPagination}/>
+                </Col>
+            </Row>
         );
     }
 }
