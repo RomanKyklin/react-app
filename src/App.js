002776b68f329e4401383e3ed29ed184e4d9e08d
Roman Kuklin <sk.trac@yandex.ru>
2019-07-03 09:33:08 +0500

    add auth

--- src/App.js
+++ src/App.js
@@ -4,16 +4,40 @@ import './App.css';
 import Navbar from "./components/Navbar";
 import {Layout} from 'antd';
 import Images from "./components/Images";
-
+import axios from 'axios';
 const {Content, Footer} = Layout;
+const _ = require('lodash');
 
 class App extends Component {
     state = {
-        term: ''
+        term: '',
     };
 
     handleSearch = (term) => {
         this.setState({term: term});
+    };
+
+    componentWillMount() {
+       this.auth();
+    }
+
+    auth = () => {
+        const urlParams = new URLSearchParams(window.location.search);
+        if(urlParams.get('code')) {
+            axios.post('https://unsplash.com/oauth/token', {
+                client_id: process.env.REACT_APP_UNSPLASH_API_KEY,
+                client_secret: process.env.REACT_APP_UNSPLASH_SECRET_KEY,
+                redirect_uri: 'http://localhost:3000',
+                code: _.get(urlParams.get('code'), '', ''),
+                grant_type: 'authorization_code'
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.log(error)
+            });
+        } else {
+            window.location.href = "https://unsplash.com/oauth/authorize?client_id=503ff9ab15e3bb7ec386d6f9339a43043cd320c465dc6948160c9e698323b820&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&response_type=code&scope=public";
+        }
     };
 
     render() {
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,7 +1,6 @@
 import React, {Component} from "react";
 import axios from 'axios';
 import {Row, Col, Alert, Pagination} from 'antd';
-
 const _ = require('lodash');
 
 export default class Images extends Component {
